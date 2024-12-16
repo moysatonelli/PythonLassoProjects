5c161079a6352af326ebd1d76d9fb0a60ed3a58e
moysatonelli <moysato.br@gmail.com>
2024-12-16 13:35:25 -0300

    Revert "Initial commit – first exercise"
    This reverts commit 17ce79b1bb553cfb2f9aafd4683c33164ad51d8b.

--- understanding_ties.py
+++
@@ -1,7 +0,0 @@
-#Ana está desenvolvendo um programa que precisa processar uma lista de 5 nomes de clientes para gerar relatórios mensais.
-#Para isso, ela precisa escrever um programa que percorra a lista de nomes e exiba cada cliente.
-
-clientes = ["João", "Maria", "Carlos", "Ana", "Beatriz"]
-
-for cliente in clientes:
-    print(cliente)
